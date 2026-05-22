ae5fe120a739cb245137259b729c5a004c760b9a
tmaeda <tmaeda@student.42tokyo.jp>
2026-05-22 22:51:51 +0900

    done

--- test/ex6/ft_count_harvest_recursive.py
+++ test/ex6/ft_count_harvest_recursive.py
@@ -1,6 +1,6 @@
 def ft_count_harvest_recursive():
     days = int(input("Days until harvest: "))
-    def count_day(day)
+    def count_day(day):
         if day <= days:
             print("Day", day)
             count_day(day + 1)
